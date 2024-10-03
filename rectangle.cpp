62816c09e67a08b4ec24e575cbba71bbb808e40b
Stephen <stivegesh@bk.ru>
2024-10-03 15:19:15 +0300

    added new file

---
+++ rectangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int area(int a, int b) {
+    return a * b;
+}
+
+int perimeter(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << "area: " << area(a, b) << " perimeter: " << perimeter(a,b);
+
+}
+
